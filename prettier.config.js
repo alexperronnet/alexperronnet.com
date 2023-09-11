cbf1b3dd91046b49f812bc9a1f0f577570c67b35
alexperronnet <github.ayfb9@lxprnt.com>
2023-09-12 01:37:34 +0200

    feat: project configuration

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-pkg'],
+}
---
+++ src/app/uses/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Uses',
+}
+
+export default function Uses() {
+  return <main>Uses</main>
+}
